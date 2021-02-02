b2fab47161e6a6d3c22d73fce1f14dfc079e6fe5
auctumnus <auctumnus@users.noreply.github.com>
2021-02-02 01:54:25 -0600

    oops its broken on non-esnext runtimes, hopefully this fixes it

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import replacers from './data/xsampa2ipa.json'
 
+// from @developit/replaceall
+// would just import it, but it refuses to work?
+// @ts-ignore
+const replaceAll = ''.replaceAll
+  ? (str: string, find: string, rep: string) => str.replaceAll(find, rep)
+  : (str: string, find: string, rep: string) => str.split(find).join(rep)
+
 const replacer = (a: string, pair: string[]) => {
   const [xs, ipa] = pair
-  return a.replaceAll(xs, ipa)
+  return replaceAll(a, xs, ipa)
 }
 
 export const ipa = (xsampa: string) => replacers.reduce(replacer, xsampa)
